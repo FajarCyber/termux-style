d97ec3ab40f493207a7958b1d5e66bc84f729647
TimeEror123 <67280289+TimeEror123@users.noreply.github.com>
2020-07-20 22:37:31 +0700

    Update Termux.sh

--- Termux.sh
+++ Termux.sh
@@ -26,10 +26,10 @@ read -p "[•] MASUK SCRIPT" read tr
 clear
 sleep 1
 figlet "TOOLS"
-echo "$red ===================================================="
-echo "$red |    $cyan AUTHOR  : FAJAR CYBER                     |"
-echo "$red |    $cyan WHATSAP : 08882091xxx                     |"
-echo "$red ===================================================="
+echo "$red ==================================================="
+echo "$red |           $cyan AUTHOR  : FAJAR CYBER                     |"
+echo "$red |           $cyan WHATSAP : 08882091xxx                     |"
+echo "$red ==================================================="
 echo "$white |         APALU LIRIK LIRIK TOD                   |"
 echo "$white |                                                 |"
 echo "$white ==================================================="
