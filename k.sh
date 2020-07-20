6e893a729469402643197495e1b676e747b57e3b
TimeEror123 <67280289+TimeEror123@users.noreply.github.com>
2020-07-20 22:25:24 +0700

    Update and rename dk.sh to k.sh

---
+++ k.sh
@@ -0,0 +1,90 @@
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+lightgreen='\e[1;32m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+clear
+sleep 1
+figlet "TOOLS"
+echo "$red ================================================="
+echo "$red |       $cyan AUTHOR  : FAJAR CYBER             |"
+echo "$red |       $cyan WHATSAP : 08882091xxx             |"
+echo "$red ================================================="
+echo "$white |         APALU LIRIK LIRIK TOD               |"
+echo "$white |                                             |"
+echo "$white ==============================================="
+echo "$cyan SILAKAN PILIH SCRIPT"
+echo
+echo "$green 1.DARK FB                          $cyan [ON]"
+echo "$green 2.HACK WHATSAP                     $cyan [ON]"
+echo "$green 3.TOOLS HACK FB                    $cyan [ON]"
+echo "$green 4.PHISING AKUN MOBILE LEGENDS / FF $cyan [ON]"
+echo "$green 5.HACK PULSA                       $cyan [ON]"
+echo "$green 6.HACK FREE FIRE                   $cyan [ON]"
+echo "$green 7.MBF                              $cyan [ON]"
+echo "$green 99.KELUAR / EXIT "
+echo
+read -p "PILIH : " bro
+
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+sh dark.sh
+fi
+
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+git clone https://github.com/Fajar-24/HACKWHATSAPP
+cd HACKWHATSAPP
+sh WhatSapp.sh
+fi
+
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+git clone https://github.com/Fajar-24/CendolDawet
+cd CendolDawet
+python2 Hack.py
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+git clone https://github.com/Fajar-24/phising
+cd phising
+sh v2.sh
+fi
+
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+git clone https://github.com/Fajar-24/HACKPULSA
+cd HACKPULSA
+sh HACKPULSA.sh
+fi
+
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+git clone https://github.com/Fajar-24/FreeFire
+cd FreeFire
+sh Fire.sh
+fi
+
+
+if [ $bro = 7 ] || [ $bro = 7 ]
+then
+git clone https://github.com/Fajar-24/Dark1.0
+cd Dark1.0
+sh Dark.sh
+fi
+
+
+if [ $bro = 99 ] || [ $bro = 99 ]
+then
+sh j.sh
+fi
