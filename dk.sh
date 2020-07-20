d578ca0346d167255099a581771defdbaa3f574f
TimeEror123 <67280289+TimeEror123@users.noreply.github.com>
2020-07-20 22:19:47 +0700

    Create dk.sh

---
+++ dk.sh
@@ -0,0 +1,20 @@
+clear
+sleep 1
+sleep 1
+echo "MAAF SCRIPT INI SEDANG DI PROSES"
+echo "SILAKAN GUNAKAN SCRIPT LAINYA"
+echo "APAKAH ANDA INGIN KEMBALI"
+echo
+echo "$cyan 1.YA"
+echo "$red 2.TIDAK"
+echo
+read -p "JADI GIMANA TOD : " bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+sh k.sh
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+fi
